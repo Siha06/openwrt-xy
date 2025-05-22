a754ba3f891f1f598752d45ee73cae2ccddb1af4
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-05-22 19:09:40 +0800

    Update lean-x86-23.05.sh

--- script/lean-x86-23.05.sh
+++ script/lean-x86-23.05.sh
@@ -1,6 +1,6 @@
-sed -i 's/192.168.1.1/172.168.1.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/172.168.1.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/192.168.1.1/172.168.1.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.86.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.86.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/192.168.86.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
