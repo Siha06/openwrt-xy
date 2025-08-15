343a18ca2e1d08c1585691fac02d94884386e6d7
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-08-15 14:23:25 +0800

    Update lean-mini.sh

--- script/lean-mini.sh
+++ script/lean-mini.sh
@@ -1,6 +1,6 @@
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/10.3.2.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/10.3.2.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/10.3.2.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -10,5 +10,5 @@ sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-sett
 sed -i '/shadow/d' package/lean/default-settings/files/zzz-default-settings
 sed -i 's#mirrors.tencent.com/lede#mirrors.pku.edu.cn/immortalwrt#g' package/lean/default-settings/files/zzz-default-settings
 
-rm -rf feeds/luci/applications/luci-app-eqos
-git clone --depth 1 https://github.com/wiwizcom/WiFiPortal.git package/WiFiPortal
+#rm -rf feeds/luci/applications/luci-app-eqos
+#git clone --depth 1 https://github.com/wiwizcom/WiFiPortal.git package/WiFiPortal
