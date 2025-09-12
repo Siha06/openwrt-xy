035b1fa2c1533fb803f405ff695c875c78aa2ceb
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-09-12 17:04:32 +0800

    Update 199-rockchip.sh

--- patch/lean/199-rockchip.sh
+++ patch/lean/199-rockchip.sh
@@ -21,12 +21,14 @@ uci set dropbear.@dropbear[0].Interface=''
 uci commit
 
 sed -i 's#mirrors.tencent.com/lede#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
+sed -i '/passwall/d' /etc/opkg/distfeeds.conf
 sed -i '/core/d' /etc/opkg/distfeeds.conf
 sed -i '/smpackage/d' /etc/opkg/distfeeds.conf
 sed -i '/kwrt/d' /etc/opkg/distfeeds.conf
 sed -i '/luci/d' /etc/opkg/distfeeds.conf
 sed -i '$a src/gz openwrt_luci https://mirrors.pku.edu.cn/immortalwrt/releases/18.06-k5.4-SNAPSHOT/packages/aarch64_cortex-a53/luci' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz others https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/rockchip/armv8/packages' /etc/opkg/customfeeds.conf
+sed -i '$a src/gz others https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.2/targets/rockchip/armv8/packages' /etc/opkg/customfeeds.conf
 
 date_version=$(date +"%Y.%m.%d")
 sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
@@ -34,11 +36,7 @@ echo "DISTRIB_REVISION='V${date_version}'" >> /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
 echo "DISTRIB_DESCRIPTION='OpenWrt  '" >> /etc/openwrt_release
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    mv /etc/my-clash /etc/openclash/core/clash_meta
-fi
-
+cp /etc/my-clash /etc/openclash/core/clash_meta
 
 #/etc/init.d/network restart
 
