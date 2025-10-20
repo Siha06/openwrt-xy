6ddb5489e2f5a45f632de3b182f68e5f32b07f1f
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-10-20 14:22:16 +0800

    Update 199-x86-lua-2.sh

--- patch/lean/199-x86-lua-2.sh
+++ patch/lean/199-x86-lua-2.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-#uci set network.lan.ipaddr=192.168.2.2
-#uci set network.lan.gateway=192.168.2.1
-#uci set network.lan.dns=192.168.2.1
-#uci set dhcp.lan.ignore=1
-#uci set network.lan.proto='dhcp'
-#uci set luci.main.mediaurlbase=/luci-static/design
-#uci commit luci
 
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
 uci set firewall.@zone[1].input='ACCEPT'
@@ -18,24 +11,21 @@ uci set dropbear.@dropbear[0].Interface=''
 
 uci commit
 
+sed -i '/ssrp/d' /etc/opkg/distfeeds.conf
+sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
+sed -i '/passwall/d' /etc/opkg/distfeeds.conf
 sed -i '/core/d' /etc/opkg/distfeeds.conf
-sed -i '/smpackage/d' /etc/opkg/distfeeds.conf
-sed -i '/kwrt/d' /etc/opkg/distfeeds.conf
-sed -i 's#24.10.0/packages/aarch64_cortex-a53/luci#packages-18.06-k5.4/x86_64/luci#g' /etc/opkg/distfeeds.conf
+sed -i '/luci/d' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz openwrt_luci https://mirrors.pku.edu.cn/immortalwrt/releases/18.06-k5.4-SNAPSHOT/packages/x86_64/luci' /etc/opkg/distfeeds.conf
+sed -i 's#mirrors.tencent.com/lede#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
 date_version=$(date +"%Y.%m.%d")
 sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
 echo "DISTRIB_REVISION='V${date_version}'" >> /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
 echo "DISTRIB_DESCRIPTION='OpenWrt  '" >> /etc/openwrt_release
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    tar -zxf /etc/clash-linux-amd64.tar.gz -C /etc/openclash/core/
-    mv /etc/openclash/core/clash /etc/openclash/core/clash_meta
-    rm -rf /etc/clash-linux-amd64.tar.gz
-fi
+cp /etc/my-clash /etc/openclash/core/clash_meta
 
-#sed -i 's/eth0/eth99/g' /etc/config/network
 
 # 根据网卡数量配置网络
 eth_count=0
