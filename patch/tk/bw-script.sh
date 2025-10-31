acf56a017819998280de8fefcf41f11511aafc3f
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-11-01 01:15:26 +0800

    Update bw-script.sh

--- patch/tk/bw-script.sh
+++ patch/tk/bw-script.sh
@@ -1,8 +1,8 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.5.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/TikTok/g' package/base-files/files/bin/config_generate
-sed -i 's/LEDE/TikTok/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/luci/bin/config_generate
+sed -i 's/LEDE/TikTok/g' package/base-files/luci/bin/config_generate
 sed -i 's/LEDE/TikTok/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
 sed -i 's#mirrors.tencent.com/lede#mirrors.pku.edu.cn/immortalwrt#g' package/lean/default-settings/files/zzz-default-settings
