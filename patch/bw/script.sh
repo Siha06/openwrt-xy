42b31c1c515e62f5f6ee971d09bd5480f7ccfeb9
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-11-26 01:12:35 +0800

    Update script.sh

--- patch/bw/script.sh
+++ patch/bw/script.sh
@@ -1,3 +1,6 @@
+rm -rf package/lean/autocore/files/arm/index.htm
+#sed -i '/files\/etc\/index\.htm/d' package/lean/default-settings/files/zzz-default-settings
+
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.5.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
