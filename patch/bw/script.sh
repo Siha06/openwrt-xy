49970fc68dd6eb8a5960ed195c76ffc6d341565b
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-11-27 02:11:34 +0800

    Update script.sh

--- patch/bw/script.sh
+++ patch/bw/script.sh
@@ -1,5 +1,5 @@
 #rm -rf package/lean/autocore/files/arm/index.htm
-sed -i '/files\/arm\/index\.htm/d' package/lean/autocore/Makefile
+#sed -i '/files\/arm\/index\.htm/d' package/lean/autocore/Makefile
 
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.5.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
