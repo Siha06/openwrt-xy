cb3ef2a1c9defcc38cbf0432a35fcb27a7c53662
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-01-13 14:05:45 +0800

    Update lean-762x.sh

--- script/lean-762x.sh
+++ script/lean-762x.sh
@@ -2,7 +2,7 @@ sed -i 's/192.168.1.1/10.3.2.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/10.3.2.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/10.3.2.1/g' package/base-files/luci/bin/config_generate
-#sed -i 's/LEDE/OpenWrt/g' package/base-files/luci/bin/config_generate
+sed -i 's/LEDE/OpenWrt/g' package/base-files/luci/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 mv $GITHUB_WORKSPACE/patch/lean/199-diy-762x package/base-files/files/etc/uci-defaults/zz-diy
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
