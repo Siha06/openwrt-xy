9bb5d50f50a272583d464c4b0e3725c5dc41b1be
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-01-19 15:17:02 +0800

    Update lean-762x.sh

--- script/lean-762x.sh
+++ script/lean-762x.sh
@@ -6,7 +6,7 @@ sed -i 's/LEDE/OpenWrt/g' package/base-files/luci/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 mv $GITHUB_WORKSPACE/patch/lean/199-diy-762x package/base-files/files/etc/uci-defaults/zz-diy
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
-#sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
+sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
 sed -i '/shadow/d' package/lean/default-settings/files/zzz-default-settings
 sed -i 's#mirrors.tencent.com/lede#mirrors.pku.edu.cn/immortalwrt#g' package/lean/default-settings/files/zzz-default-settings
 
